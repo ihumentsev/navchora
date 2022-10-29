15d597ba2bb3d97ae74d495fe9ae6e5c28e2d630
ihumentsev <a.ihumentsev@gmail.com>
2022-10-29 20:15:14 +0200

    .

--- src/components/ContactItems/ContactItems.jsx
+++ src/components/ContactItems/ContactItems.jsx
@@ -3,14 +3,14 @@ import { ContactLink, ContactList } from './ContactItem.styled';
 export default function ContactItems() {
   return (
     <ContactList className="contact-list">
-      {/* <li className="contact-item">
+      <li className="contact-item">
         <ContactLink
           className="contact-link"
           href="mailto:info@navchora.com.ua"
         >
           info@navchora.com.ua
         </ContactLink>
-      </li> */}
+      </li>
       <li className="contact-item">
         <ContactLink className="contact-link" href="tel:+380507408745">
           +38 (050) 740 8 745
